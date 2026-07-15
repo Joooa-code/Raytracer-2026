b1513f1cc02bf77d5f3a8272b592f5e765e1731e
joooa <wangxinbei@sjtu.edu.cn>
2026-07-15 23:33:34 +0800

    feat book2/image23

--- src/main.rs
+++ src/main.rs
@@ -466,8 +466,153 @@ fn cornell_smoke() {
     let world: Arc<dyn Hittable + Send + Sync> = Arc::new(world);
     cam.render(&world);
 }
+pub fn final_scene(image_width: usize, samples_per_pixel: usize, max_depth: usize) {
+    let mut boxes1 = HittableList::default();
+
+    let ground = Arc::new(Lambertian::new_color(Color::new(0.48, 0.83, 0.53)));
+    let boxes_per_side = 20;
+
+    for i in 0..boxes_per_side {
+        for j in 0..boxes_per_side {
+            let w = 100.0;
+            let x0 = -1000.0 + i as f64 * w;
+            let z0 = -1000.0 + j as f64 * w;
+            let y0 = 0.0;
+            let x1 = x0 + w;
+            let y1 = random_f64_range(1.0, 101.0);
+            let z1 = z0 + w;
+
+            boxes1.add(Arc::new(Quad::box3d(
+                &Point3::new(x0, y0, z0),
+                &Point3::new(x1, y1, z1),
+                ground.clone(),
+            )));
+        }
+    }
+
+    let mut world = HittableList::default();
+
+    // BVH
+    let object_len = boxes1.objects.len();
+    let bvh1: Arc<dyn Hittable> = Arc::new(BVHNode::new(&mut boxes1.objects, 0, object_len));
+    world.add(bvh1);
+
+    // light
+    let light = Arc::new(DiffuseLight::new_color(Color::new(7.0, 7.0, 7.0)));
+    world.add(Arc::new(Quad::new(
+        Point3::new(123.0, 554.0, 147.0),
+        Vec3::new(300.0, 0.0, 0.0),
+        Vec3::new(0.0, 0.0, 265.0),
+        light,
+    )));
+
+    // moving sphere
+    let center1 = Point3::new(400.0, 400.0, 200.0);
+    let center2 = center1 + Vec3::new(30.0, 0.0, 0.0);
+    let sphere_material = Arc::new(Lambertian::new_color(Color::new(0.7, 0.3, 0.1)));
+    world.add(Arc::new(Sphere::new_mov(
+        center1,
+        center2,
+        50.0,
+        sphere_material,
+    )));
+
+    // glass sphere
+    world.add(Arc::new(Sphere::new(
+        Point3::new(260.0, 150.0, 45.0),
+        50.0,
+        Arc::new(Dielectric::new(1.5)),
+    )));
+
+    // metal sphere
+    world.add(Arc::new(Sphere::new(
+        Point3::new(0.0, 150.0, 145.0),
+        50.0,
+        Arc::new(Metal::new(Color::new(0.8, 0.8, 0.9), 1.0)),
+    )));
+
+    // blue volume
+    let boundary = Arc::new(Sphere::new(
+        Point3::new(360.0, 150.0, 145.0),
+        70.0,
+        Arc::new(Dielectric::new(1.5)),
+    ));
+
+    world.add(boundary.clone());
+    world.add(Arc::new(ConstantMedium::new_color(
+        boundary,
+        0.2,
+        Color::new(0.2, 0.4, 0.9),
+    )));
+
+    // global fog
+    let boundary = Arc::new(Sphere::new(
+        Point3::zero(),
+        5000.0,
+        Arc::new(Dielectric::new(1.5)),
+    ));
+
+    world.add(Arc::new(ConstantMedium::new_color(
+        boundary,
+        0.0001,
+        Color::new(1.0, 1.0, 1.0),
+    )));
+
+    // earth
+    let earth_texture = Arc::new(ImageTexture::new("earthmap.jpg"));
+    let earth_material = Arc::new(Lambertian::new(earth_texture));
+
+    world.add(Arc::new(Sphere::new(
+        Point3::new(400.0, 200.0, 400.0),
+        100.0,
+        earth_material,
+    )));
+
+    // noise sphere
+    let pertext = Arc::new(NoiseTexture::new(0.2));
+    world.add(Arc::new(Sphere::new(
+        Point3::new(220.0, 280.0, 300.0),
+        80.0,
+        Arc::new(Lambertian::new(pertext)),
+    )));
+
+    // 1000 small spheres
+    let mut boxes2 = HittableList::default();
+
+    let white = Arc::new(Lambertian::new_color(Color::new(0.73, 0.73, 0.73)));
+
+    for _ in 0..1000 {
+        boxes2.add(Arc::new(Sphere::new(
+            Point3::random_range(0.0, 165.0),
+            10.0,
+            white.clone(),
+        )));
+    }
+    let object_len = boxes2.objects.len();
+    let bvh2: Arc<dyn Hittable + Send + Sync> =
+        Arc::new(BVHNode::new(&mut boxes2.objects, 0, object_len));
+    let rotated = Arc::new(RotateY::new(bvh2, 15.0));
+    let translated = Arc::new(Translate::new(rotated, Vec3::new(-100.0, 270.0, 395.0)));
+    world.add(translated);
+
+    // camera
+    let mut cam = Camera::default();
+    cam.aspect_ratio = 1.0;
+    cam.image_width = image_width;
+    cam.samples_per_pixel = samples_per_pixel;
+    cam.max_depth = max_depth;
+    cam.background = Color::zero();
+    cam.vfov = 40.0;
+    cam.lookfrom = Point3::new(478.0, 278.0, -600.0);
+    cam.lookat = Point3::new(278.0, 278.0, 0.0);
+    cam.vup = Vec3::new(0.0, 1.0, 0.0);
+    cam.defocus_angle = 0.0;
+    let world: Arc<dyn Hittable + Send + Sync> = Arc::new(world);
+
+    cam.render(&world);
+}
 fn main() {
-    let scene = 8;
+    let scene = 98;
 
     match scene {
         1 => bouncing_spheres(),
@@ -478,6 +623,7 @@ fn main() {
         6 => simple_light(),
         7 => cornell_box(),
         8 => cornell_smoke(),
-        _ => {}
+        9 => final_scene(800, 10000, 40),
+        _ => final_scene(400, 250, 4),
     }
 }
